3eab43af8ec5a1a870bf354eac154a9ad55e5060
Alex Olson <alexkellenolson@gmail.com>
2018-02-12 23:11:58 +0100

    initial code.

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const request = require('request');
+
+const {
+  ABC_APP_ID,
+  ABC_APP_KEY,
+  ABC_CLUB_NUMBER,
+  ABC_API_URL,
+} = process.env;
+
+
+function makeRequest(resource, page, accumulator, done) {
+  request.get(`${ABC_API_URL}/${ABC_CLUB_NUMBER}/${resource}`, {
+    qs: { page },
+    headers: {
+      accept: 'application/json',
+      app_id: ABC_APP_ID,
+      app_key: ABC_APP_KEY,
+    },
+  }, (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return done(err);
+    }
+
+    const res = JSON.parse(body);
+
+    const {
+      status: {
+        nextPage,
+      },
+      [resource]: data = [],
+    } = res;
+
+    accumulator.push(...data);
+
+    if (nextPage) {
+      return makeRequest(resource, nextPage, accumulator, done);
+    }
+
+    return done(null, accumulator)
+  });
+}
+
+makeRequest('members', 1, [], (membersErr, members) => {
+  if (membersErr) {
+    return console.log(membersErr);
+  }
+
+  return makeRequest('prospects', 1, [], (prospectsErr, prospects) => {
+    if (prospectsErr) {
+      return console.log(prospectsErr);
+    }
+
+    console.log({ members, prospects });
+  });
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+# ABCFinancial => AWeber Data Pipeline
+Pipes members and prospects data from ABCFinancial gym management API into custom list names in AWeber.
+
+## Files
+- [index.js](index.js) Make requests to the ABCFinancial API for members and prospects data.
+- [authorize-aweber.py](authorize-aweber.py) Obtain an OAuth access token for the Aweber API.
